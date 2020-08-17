14536982dad51255ba3753f3f6385fc39c838722
David R <davidranger77@outlook.com>
2020-08-17 15:20:15 -0400

    commentaire seulements

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+afficher l'heure actuelle, le nom de chaque joueur et les 
+poissons et insectes manquants disponible en ce momant 
+pour chacun d'eux.
+'''
+
+
+
+'''
+Afficher un menu de choix d'opérations
+1) inscrire une nouvelle prise
+2) annuler une prise inscrite
+3) ajouter une créature à la liste officielle
+4) enlever une créature à la liste officielle
+5) ajouter un joueur
+6) effacer un joueur
+'''
