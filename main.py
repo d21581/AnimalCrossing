2b93b98809cf08a12a0006d3cbe97ddddb083f9b
David R <davidranger77@outlook.com>
2020-08-21 16:22:33 -0400

    encore formatage liste insecte (complète)

--- main.py
+++ main.py
@@ -9,48 +9,62 @@ import datetime
 heure_actuelle = datetime.datetime.now()
 
 print('**********************', heure_actuelle, '************************')
+print('**********************', heure_actuelle, '************************')
+print('**********************', heure_actuelle, '************************')
+print('**********************', heure_actuelle, '************************')
+print('**********************', heure_actuelle, '************************')
+print('**********************', heure_actuelle, '************************')
+print('**********************', heure_actuelle, '************************')
 
 with open('bugs_p2.txt', 'r') as fichier_bugs:
 
-	number_of_lines = 521#len(fichier_bugs.readlines())
+	total_number_of_lines = 521 #len(fichier_bugs.readlines())
 
-	print(number_of_lines)
+	print(total_number_of_lines)
 
-	lines_to_read = 9
+	lines_to_read = 8
 
 	compteur_lignes = 0
 
-	nouvelle_ligne = ''
-
 	nouveau_fichier = ''
 
-	while compteur_lignes < number_of_lines:
+	while compteur_lignes < total_number_of_lines:
 
-		compteur_lignes_block = 1
+		compteur_lignes_block = 0
+
+		nouvelle_ligne = ''
 
 		while compteur_lignes_block < lines_to_read: 
 
 			info_ligne = fichier_bugs.readline()
 
-			print(str(compteur_lignes) + '/' + str(number_of_lines), compteur_lignes_block, info_ligne + '')
+			info_ligne = info_ligne.replace("\n", "")
 
-			if compteur_lignes_block == 1:
+			#print(str(compteur_lignes) + '/' + str(total_number_of_lines), compteur_lignes_block, info_ligne + '')
 
-				nouvelle_ligne = info_ligne + ';'
-
-			if compteur_lignes_block == 3:
+			if compteur_lignes_block < 6:
 
 				nouvelle_ligne = nouvelle_ligne + info_ligne + ';'
 
-			if compteur_lignes_block == 9:
+			if compteur_lignes_block == 6:
 
-				nouvelle_ligne = nouvelle_ligne + '\n'
+				if info_ligne != '': # il y a un commentaire
+
+					nouvelle_ligne = nouvelle_ligne + info_ligne + ";\n"
+
+				else: #fin du bloc parce qu'il n'y a pas de commentaires
+
+					nouvelle_ligne = nouvelle_ligne + ";\n"
+
+					break;
 
 			compteur_lignes_block = compteur_lignes_block + 1
 
 			compteur_lignes = compteur_lignes + 1
 
 		nouveau_fichier = nouveau_fichier + nouvelle_ligne
+
+		print('nouvelle ligne: ' + nouvelle_ligne)
 '''
 			if compteur_lignes_block == 3:
 
