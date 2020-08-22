94b1bae7af525cc5184837832456de597879a727
David R <davidranger77@outlook.com>
2020-08-22 11:39:39 -0400

    affichage heure en haut bon format

--- main.py
+++ main.py
@@ -3,19 +3,7 @@
 ###################
 
 import datetime
-
-
 '''
-afficher l'heure actuelle, le nom de chaque joueur et les 
-poissons et insectes manquants disponible en ce momant 
-pour chacun d'eux.
-'''
-
-
-heure_actuelle = datetime.datetime.now()
-
-print('**********************', heure_actuelle, '************************')
-
 with open('fish.txt', 'r') as fichier_bugs:
 
 	total_number_of_lines = 521 #len(fichier_bugs.readlines())
@@ -65,18 +53,39 @@ with open('fish.txt', 'r') as fichier_bugs:
 		nouveau_fichier = nouveau_fichier + nouvelle_ligne
 
 		#print('nouvelle ligne: ' + nouvelle_ligne)
-
-
-
 '''
-Afficher un menu de choix d'opérations
-1) inscrire une nouvelle prise
-2) annuler une prise inscrite
-3) ajouter une créature à la liste officielle
-4) enlever une créature à la liste officielle
-5) ajouter un joueur
-6) effacer un joueur
-'''
+
+
+########################
+# Les variables globales
+########################
+
+heure_actuelle_raw = datetime.datetime.now()
+
+heure_format_24 = heure_actuelle_raw.hour
+
+mois_format_12 = heure_actuelle_raw.month
+
+jour_format_30 = heure_actuelle_raw.day
+
+jour_format_long = heure_actuelle_raw.strftime("%A")
+
+heure_format_long = heure_actuelle_raw.strftime("%X")[:-3]
+
+noms_mois = {
+	1: "janvier",
+	2: "février",
+	3: "mars",
+	4: "avril",
+	5: "mai",
+	6: "juin",
+	7: "juillet",
+	8: "août",
+	9: "septembre",
+	10: "octobre",
+	11: "novembre",
+	12: "décembre"
+}
 
 ###############
 # Les fonctions
@@ -115,10 +124,35 @@ print('ici')
 print(test[4])
 
 
+#def nommer_mois(mois):
 
 
 
 
+###############
+# Le programme
+###############
+
+'''
+afficher l'heure actuelle, le nom de chaque joueur et les 
+poissons et insectes manquants disponible en ce momant 
+pour chacun d'eux.
+'''
+
+
+
+
+print('**********************', jour_format_30, noms_mois[mois_format_12], heure_format_long, '************************')
+
+'''
+Afficher un menu de choix d'opérations
+1) inscrire une nouvelle prise
+2) annuler une prise inscrite
+3) ajouter une créature à la liste officielle
+4) enlever une créature à la liste officielle
+5) ajouter un joueur
+6) effacer un joueur
+'''
 
 
 
